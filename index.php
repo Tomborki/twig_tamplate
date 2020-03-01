ebaf3728350ba249cfc156a65af3381279081d51
Tomáš Borkovec <tomasborki@gmail.com>
2020-03-01 20:47:44 +0100

    weborky tamplate

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require_once('twig-init.php');
 
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
 
-$loader = new FilesystemLoader(__DIR__ . '/templates');
-$twig = new Environment($loader);
 
-echo $twig->render('index.html', ['name' => 'John Doe', 
-    'occupation' => 'gardener']);
+
+echo $twig->render('index.html', [
+    'name' => 'John Doe', 
+    'occupation' => 'gardener',
+    ]);
